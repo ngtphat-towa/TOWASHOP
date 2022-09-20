c976c457d8373fcade42cf5fdf46469bb4a3c819
ElHanaya <89678375+phatnguyen1710@users.noreply.github.com>
2022-09-20 11:43:01 +0700

    add more supplier stored procedures

--- TowaShopSQLServer/SP/spSupplier_Delete.sql
+++ TowaShopSQLServer/SP/spSupplier_Delete.sql
@@ -13,28 +13,22 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+USE TOWASHOP;
+GO
 -- =============================================
 -- Author:		<Author,,Name>
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-CREATE PROCEDURE dbo.spSupplier_Insert
+CREATE PROCEDURE dbo.spSupplier_Delete
 	-- Add the parameters for the stored procedure here
-	@SUPPLIER_NAME NVARCHAR(30),
-	@CONTACT_NAME NVARCHAR(45) = NULL,
-	@CONTACT_PHONE NVARCHAR(20),
-	@ADDRESS NVARCHAR(50)= NULL,
-	@CITY NVARCHAR(20)= NULL,
-	@COUNTRY NVARCHAR(20) = NULL
+	@SLID nvarchar(15)
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-	DECLARE @NEWSLID nvarchar(8);
-	SELECT @NEWSLID =  ('SL'+CAST((SELECT Cast(SUBSTRING((Select Max(SUPPLIERID) from dbo.SUPPLIERS S),3,6) AS INT)+1) as varchar(6)));
-    -- Insert statements for procedure here
-	INSERT INTO DBO.SUPPLIERS (SUPPLIERID,SUPPLIER_NAME, CONTACT_NAME, CONTACT_PHONE, "ADDRESS", CITY, COUNTRY)
-	VALUES (@NEWSLID,@SUPPLIER_NAME, @CONTACT_NAME,@CONTACT_PHONE, @ADDRESS,@CITY,@COUNTRY);
+	DELETE FROM  DBO.SUPPLIERS 
+	WHERE SUPPLIERID  = @SLID;
 END
 GO
