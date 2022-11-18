9086f19e62a13064aee32d80365baa83f72e8cf5
ElHanaya <89678375+phatnguyen1710@users.noreply.github.com>
2022-11-18 14:10:42 +0700

    Add Common Services and Project Structure

---
+++ TOWALibrary/Services/CommonServices/GlobalConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOWALibrary.Models.Account.Users;
+
+namespace TOWALibrary.Services.CommonServices
+{
+   public static class GlobalConfig
+    {
+        private static AccountModel _CurrentUser;
+
+        public static AccountModel CurrentUser { get => _CurrentUser; set => _CurrentUser = value; }
+    }
+}
