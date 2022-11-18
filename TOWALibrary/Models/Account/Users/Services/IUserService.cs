6723324a65fab9fb28490dba26e0d1260abad657
ElHanaya <89678375+phatnguyen1710@users.noreply.github.com>
2022-11-18 14:15:55 +0700

    Move UserServices to Services/AccountModel

--- TOWALibrary/Models/Account/Users/Services/IUserService.cs
+++ TOWALibrary/Models/Account/Users/Services/IUserService.cs
@@ -11,18 +11,18 @@ namespace TOWALibrary.Models.Accounts.Users.Services
 {
     public interface IUserService
     {
-        /// <summary>
-        /// Take the user and the password to validate the login
-        /// </summary>
-        /// <param name="username"></param>
-        /// <param name="password"></param>
-        /// <returns></returns>
-        bool Validate(string username, string password);
-        /// <summary>
-        /// Using role for the view authenitcation 
-        /// </summary>
-        /// <returns></returns>
-        MainViewType GetRoleView();
+        ///// <summary>
+        ///// Take the user and the password to validate the login
+        ///// </summary>
+        ///// <param name="username"></param>
+        ///// <param name="password"></param>
+        ///// <returns></returns>
+        //bool Validate(string username, string password);
+        ///// <summary>
+        ///// Using role for the view authenitcation 
+        ///// </summary>
+        ///// <returns></returns>
+        //MainViewType GetRoleView();
         
     }
 }
---
+++ TOWALibrary/Services/ModelServices/UserServices/IAccountModelServices.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOWALibrary.Repositories.Accounts.Users;
+using TOWALibrary.Views;
+
+namespace TOWALibrary.Services.ModelServices.UserServices
+{
+    public interface IAccountModelServices: IAccountRepository
+    {
+        /// <summary>
+        /// Take the user and the password to validate the login
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        bool Validate(string username, string password);
+        /// <summary>
+        /// Using role for the view authenitcation 
+        /// </summary>
+        /// <returns></returns>
+        MainViewType GetRoleView();
+    }
+}
